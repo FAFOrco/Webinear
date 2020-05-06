86bfcf8b8644bab7ebe0eb6ee542ae96c1d2e1bc
FAF <fernando_figliola@itrsa.com.ar>
2020-05-06 10:34:04 -0300

    Actualizacion 06/05/2020

--- Mobile/Generales/OpenAUT.UserCode.cs
+++ Mobile/Generales/OpenAUT.UserCode.cs
@@ -20,9 +20,9 @@ using Ranorex.Core;
 using Ranorex.Core.Repository;
 using Ranorex.Core.Testing;
 
-namespace Mobile
+namespace Mobile.Generales
 {
-    public partial class Recording1
+    public partial class OpenAUT
     {
         /// <summary>
         /// This method gets called right after the recording has been started.
